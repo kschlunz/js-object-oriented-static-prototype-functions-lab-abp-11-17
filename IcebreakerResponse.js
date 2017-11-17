82acbc0f09dff8245bd5d25a27ca83f86518f48c
Kate Schlunz <schlunzk@gmail.com>
2017-11-17 17:12:44 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+User.BatchCreate = function(data){
+  return data.map(function(userData){
+    return new IcebreakerResponse(userData[0], userData[1])
+  })
+}
